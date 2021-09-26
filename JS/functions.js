264a82512aa3e31436825c99acf20dbfe269d627
LumberMaster <artemsertyc@gmail.com>
2021-09-27 02:15:30 +0300

    Сайт адаптирован под телефоны
    Signed-off-by: LumberMaster <artemsertyc@gmail.com>

--- JS/functions.js
+++ JS/functions.js
@@ -22,7 +22,25 @@ function Request(url, type="GET") {
         return xhr.responseText; // responseText -- текст ответа.
     }
 }
+function TestRequest(url, type="GET") {
+    // 1. Создаём новый объект XMLHttpRequest
+    var xhr = new XMLHttpRequest();
 
+    // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
+    xhr.open(type, url, false);
+
+    // 3. Отсылаем запрос
+    xhr.send();
+
+    // 4. Если код ответа сервера не 200, то это ошибка
+    if (xhr.status != 200) {
+        // обработать ошибку
+        console.log( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+    } else {
+        // вывести результат
+        return xhr.responseText; // responseText -- текст ответа.
+    }
+}
 function Load_MyWork() {
 
     let Arr = JSON.parse(Request('./JSON/Projects.json')),
