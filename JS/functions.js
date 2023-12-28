650bdfe9afa28abb0c63d7392b46dedc34bdd6a0
Artem Gavrilov <artemsertyc@gmail.com>
2023-12-28 14:12:41 +0300

    Убрал ссылки на фейсбук и инстаграм Signed-off-by: Artem Gavrilov <artemsertyc@gmail.com>

--- JS/functions.js
+++ JS/functions.js
@@ -30,23 +30,12 @@ function Request(url, type="GET") {
     }
 }
 function TestRequest(url, type="GET") {
-    // 1. Создаём новый объект XMLHttpRequest
     var xhr = new XMLHttpRequest();
-
-    // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
     xhr.open(type, url, false);
-
-    // 3. Отсылаем запрос
     xhr.send();
-
-    // 4. Если код ответа сервера не 200, то это ошибка
-    if (xhr.status != 200) {
-        // обработать ошибку
-        console.log( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
-    } else {
-        // вывести результат
-        return xhr.responseText; // responseText -- текст ответа.
-    }
+    
+    if (xhr.status != 200) console.log( xhr.status + ': ' + xhr.statusText ); 
+    else return xhr.responseText; 
 }
 function Load_MyWork() {
 
